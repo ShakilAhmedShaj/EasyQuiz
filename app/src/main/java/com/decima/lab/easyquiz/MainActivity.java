8e523ad1631ff818adaa3505159b9ae83f6f0f70
SHAJ <shakilahmedshaj@gmail.com>
2019-10-08 21:46:16 +0600

    check ans done

--- app/src/main/java/com/decima/lab/easyquiz/MainActivity.java
+++ app/src/main/java/com/decima/lab/easyquiz/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.decima.lab.easyquiz.data.AnswerListAsyncResponse;
 import com.decima.lab.easyquiz.data.QuestionBank;
@@ -75,14 +76,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 updateQuestion();
                 break;
             case R.id.true_button:
-//                checkAnswer(true);
+                checkAnswer(true);
 //                updateQuestion();
                 break;
             case R.id.false_button:
-//                checkAnswer(false);
+                checkAnswer(false);
 //                updateQuestion();
                 break;
         }
+
+    }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answerIsTrue = questionList.get(currentQuestionIndex).isAnswerTrue();
+
+        int toastMessageId = 0;
+        if (userChooseCorrect == answerIsTrue) {
+
+            //fadeView();
+            toastMessageId = R.string.correct_answer;
+        } else {
+            //shakeAnimation();
+            toastMessageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this, toastMessageId,
+                Toast.LENGTH_SHORT)
+                .show();
+
 
     }
 
